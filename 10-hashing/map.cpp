8ac9c075bb08aa7dbf847b918ba23c3e52c1ba04
Steven Bitner <sbitner@uwf.edu>
2022-03-24 15:17:47 -0500

    Unordered map example

---
+++ 10-hashing/map.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+int main ()
+{
+	std::unordered_map<std::string,int> map;
+	map["foo"] = 12;
+
+	std::cout << map["foo"] << std::endl;
+
+	if (map.find("foo") != map.end())
+	{
+		std::cout << "found it\n";
+	}
+	else
+	{
+		std::cout << "found didn't find\n";
+	}
+	if (map.find("bar") != map.end())
+	{
+		std::cout << "found it\n";
+	}
+	else
+	{
+		std::cout << "found didn't find\n";
+	}
+
+	std::cout << map["bar"] << std::endl;
+
+	if (map.find("bar") != map.end())
+	{
+		std::cout << "found it\n";
+	}
+	else
+	{
+		std::cout << "found didn't find\n";
+	}
+	return 0;
+}
